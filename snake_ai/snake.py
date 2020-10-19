4c6a949b01a494795a4428430387fde64b92c414
kronos <maximlian.fenkart@protonmail.com>
2020-10-19 19:21:05 +0200

    snake_ai

--- snake_ai/snake.py
+++ snake_ai/snake.py
@@ -1,3 +1,53 @@
 class Snake:
-    def __init__(self, board_size, seed):
+    def __init__(self, board_size, square_size):
+        # Constants
+        self.BOARD_SIZE = board_size
+        self.SQUARE_SIZE = square_size
+
+        # Vars
+        self.score = 0
+        self.direction = ''
+        self.pos = (self.BOARD_SIZE[0] // 2, self.BOARD_SIZE[1] // 2)
+        self.apple_location = None
+        self.is_alive = False
+
+        #Start Game
+        self.init_snake('r')
+        self.init_vel('r', 1)
+        self.generate_apple()
+
+    def init_snake(self, starting_direction):
+        """
+        Initializing the snake
+            starting_direction --> The direction the snake should start facing, which ever direction it is, the head of
+            the snake will start facing this way.
+        """
+        head = [self.pos[0], self.pos[1]]
+        if starting_direction == 'u':
+            snake = [head, (head[0], head[1] + 1), (head[0], head[1] + 2)]
+        # Body is above
+        elif starting_direction == 'd':
+            snake = [head, (head[0], head[1] - 1), (head[0], head[1] - 2)]
+        # Body is to the right
+        elif starting_direction == 'l':
+            snake = [head, (head[0] + 1, head[1]), (head[0] + 2, head[1])]
+        # Body is to the left
+        elif starting_direction == 'r':
+            snake = [head, [head[0] - 2, head[1]], [head[0] - 2, head[1]]]
+
+        # Copy the Generated Snake into the snake_array and set it alive
+        self.snake_array = snake
+        self.is_alive = True
+
+
+    def init_vel(self, starting_direction, initial_velocity):
+        if initial_velocity
+
+    def update(self):
+        pass
+
+    def is_dead(self):
+        pass
+
+    def generate_apple(self):
         pass
--- snake_ai/snake_app.py
+++ snake_ai/snake_app.py
@@ -1,30 +1,46 @@
-from PyQt5 import QtGui, QtCore, QtWidgets
-from PyQt5.QtCore import Qt
+from PyQt5 import QtGui, QtWidgets, QtCore
+from PyQt5.QtWidgets import *
 import sys
-from typing import List
-from snake import *
+import snake
 
-SQUARE_SIZE = (8, 8)
+# Constants
+SQUARE_SIZE = (10, 10)
 
-class SnakeWidget(QtWidgets.QWidget):
-    def __init__(self, board_size=(50, 50)):
+
+# The Widget in which the Snake Game will be running
+class SnakeWidget(QWidget):
+    def __init__(self, board_size=(50,50)):
         super().__init__()
+
+        # Widget Config and Init
+        self.setWindowTitle("Snake Widget")
         self.board_size = board_size
-        self.setFixedSize(SQUARE_SIZE[0] * self.board_size[0], SQUARE_SIZE[1] * self.board_size[1])
+        self.setFixedSize(self.board_size[0] * SQUARE_SIZE[0], self.board_size[1] * SQUARE_SIZE[1])
         self.new_game()
 
+        # Set update Time and activate update Function
         self.timer = QtCore.QTimer(self)
         self.timer.timeout.connect(self.update)
-        self.timer.start(1000./15)
-        self.show()
-
-    def new_game(self) -> None:
-        self.snake = Snake(self.board_size, seed=0)
+        self.timer.start(100)
 
     def update(self):
-        self.repaint()
+        pass
+
+    def paintEvent(self, a0: QtGui.QPaintEvent) -> None:
+        pass
+
+    def keyPressEvent(self, a0: QtGui.QKeyEvent) -> None:
+        pass
+
+    def new_game(self):
+        # Generate a new snake
+        self.snake = snake.Snake(self.board_size, SQUARE_SIZE)
+
+
+
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    ex = SnakeWidget()
-    sys.exit(app.exec_())
+    app = QApplication(sys.argv)  # Giving it some kind of standard Setup depending on the OS
+    screen = SnakeWidget()
+    screen.show()
+    sys.exit(app.exec_()) # Makes sure our window shows up nicely and exits when we want to
