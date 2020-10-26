e526273e2851148582df09a9cf2a21348df2c766
kronos <maximilian.fenkart@protonmail.com>
2020-10-26 10:17:23 +0100

    update

--- snake_ai/snake.py
+++ snake_ai/snake.py
@@ -1,3 +1,8 @@
+from typing import Tuple, Optional, Union
+import random
+from collections import deque
+import sys
+
 class Snake:
     def __init__(self, board_size, square_size):
         # Constants
@@ -7,14 +12,12 @@ class Snake:
         # Vars
         self.score = 0
         self.direction = ''
-        self.velocity = [0, 0]
-        self.pos = (self.BOARD_SIZE[0] // 2, self.BOARD_SIZE[1] // 2)
+        self.starting_pos = (self.BOARD_SIZE[0] // 2, self.BOARD_SIZE[1] // 2)
         self.apple_location = None
         self.is_alive = False
 
         #Start Game
         self.init_snake('r')
-        self.init_vel('r', (1, 0))
         self.generate_apple()
 
     def init_snake(self, starting_direction):
@@ -23,7 +26,9 @@ class Snake:
             starting_direction --> The direction the snake should start facing, which ever direction it is, the head of
             the snake will start facing this way.
         """
-        head = [self.pos[0], self.pos[1]]
+        global snake
+        head = self.starting_pos
+
         if starting_direction == 'u':
             snake = [head, (head[0], head[1] + 1), (head[0], head[1] + 2)]
         # Body is above
@@ -34,21 +39,85 @@ class Snake:
             snake = [head, (head[0] + 1, head[1]), (head[0] + 2, head[1])]
         # Body is to the left
         elif starting_direction == 'r':
-            snake = [head, [head[0] - 2, head[1]], [head[0] - 2, head[1]]]
+            snake = [head, [head[0] - 1, head[1]], [head[0] - 2, head[1]]]
 
-        # Copy the Generated Snake into the snake_array and set it alive
-        self.snake_array = snake
+        # Copy the Generated Snake into the snake_array copy the starting direction and set it alive
+        self.snake_array = deque(snake)
+        self.direction = starting_direction
         self.is_alive = True
 
+    def update(self) -> bool:
+        global next_pos
 
-    def init_vel(self, starting_direction, initial_velocity):
-        self.velocity = initial_velocity
+        # Is snake even alive
+        if not self.is_alive:
+            return False
 
-    def update(self):
-        pass
+        # Is the direction valid?
+        direction = self.direction
+        if direction not in ('u', 'd', 'l', 'r'):
+            return False
+
+        head = self.snake_array[0]
+        # Move the Head
+        if self.direction == 'u':
+            next_pos = (head[0], head[1] - 1)
+        elif self.direction == 'd':
+            next_pos = (head[0], head[1] + 1)
+        elif self.direction == 'r':
+            next_pos = (head[0] + 1, head[1])
+        elif self.direction == 'l':
+            next_pos = (head[0] - 1, head[1])
+
+        # Is the next position we want to move to valid?
+        if self._is_valid(next_pos):
+            self.snake_array.appendleft(next_pos)
+            # If we just consumed the apple, generate a new one.
+            # No need to pop the tail of the snake since the snake is growing here
+            if next_pos == self.apple_location:
+                self.generate_apple()
+                self.score += 1
+            else:
+                self.snake_array.pop()
+
+            return True
+        else:
+            self.is_alive = False
+            return False
 
     def is_dead(self):
         pass
 
     def generate_apple(self):
-        pass
+        width = self.BOARD_SIZE[0]
+        height = self.BOARD_SIZE[1]
+
+        # Find all possibilities for a apple to be placed
+        possibilities = [divmod(i, height) for i in range(width * height) if divmod(i, height) not in self.snake_array]
+
+        # if there are any possibilities left place an apple at a random location
+        if possibilities:
+            self.apple_location = possibilities[random.randint(0, len(possibilities))]
+        else:
+            # I guess you win?
+            print('you won!')
+            pass
+
+    def _is_valid(self, next_pos) -> bool:
+        """
+        Determine whether a given position is valid.
+        Return True if the position is on the board and does not intersect the snake.
+        Return False otherwise
+        """
+        if (next_pos[0] < 0) or (next_pos[0] > self.BOARD_SIZE[0] - 1):
+            return False
+        if (next_pos[1] < 0) or (next_pos[1] > self.BOARD_SIZE[1] - 1):
+            return False
+
+        for pos in self.snake_array:
+            if pos == self.snake_array[-1]:
+                continue
+            elif pos == next_pos:
+                return False
+
+        return True
--- snake_ai/snake_app.py
+++ snake_ai/snake_app.py
@@ -1,21 +1,26 @@
 from PyQt5 import QtGui, QtWidgets, QtCore
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
 import sys
 import snake
 
 # Constants
-SQUARE_SIZE = (10, 10)
+SQUARE_SIZE = (20, 20)
+BOARD_SIZE = (20, 20)
 
 
 # The Widget in which the Snake Game will be running
 class SnakeWidget(QWidget):
-    def __init__(self, board_size=(50,50)):
+    def __init__(self, board_size, square_size):
         super().__init__()
+        # Constants
+        self.BOARD_SIZE = board_size
+        self.SQUARE_SIZE = square_size
 
         # Widget Config and Init
         self.setWindowTitle("Snake Widget")
         self.board_size = board_size
-        self.setFixedSize(self.board_size[0] * SQUARE_SIZE[0], self.board_size[1] * SQUARE_SIZE[1])
+        self.setFixedSize(self.board_size[0] * SQUARE_SIZE[0], self.BOARD_SIZE[1] * self.SQUARE_SIZE[1])
         self.new_game()
 
         # Set update Time and activate update Function
@@ -24,23 +29,68 @@ class SnakeWidget(QWidget):
         self.timer.start(100)
 
     def update(self):
-        pass
+        # Update The Snake and close the window if the snake is dead
+        if self.snake.is_alive:
+            self.snake.update()
+        else:
+            print("dead")
+            import sys
+            sys.exit(-1)
+
+        # Repaint the Screen
+        self.repaint()
 
     def paintEvent(self, a0: QtGui.QPaintEvent) -> None:
-        pass
+        painter = QtGui.QPainter()
+        painter.begin(self)
 
-    def keyPressEvent(self, a0: QtGui.QKeyEvent) -> None:
-        pass
+        self.draw_snake(painter)
+        self.draw_apple(painter)
+        painter.end()
+
+    def keyPressEvent(self, event: QtGui.QKeyEvent) -> None:
+        key_press = event.key()
+        if key_press == Qt.Key_Up and self.snake.direction != 'd':
+            self.snake.direction = 'u'
+        elif key_press == Qt.Key_Down and self.snake.direction != 'u':
+            self.snake.direction = 'd'
+        elif key_press == Qt.Key_Right and self.snake.direction != 'l':
+            self.snake.direction = 'r'
+        elif key_press == Qt.Key_Left and self.snake.direction != 'r':
+            self.snake.direction = 'l'
 
     def new_game(self):
         # Generate a new snake
         self.snake = snake.Snake(self.board_size, SQUARE_SIZE)
 
+    def draw_snake(self, painter: QtGui.QPainter) -> None:
+        # Init Painter
+        painter.setRenderHint(QtGui.QPainter.HighQualityAntialiasing)
+        painter.setPen(QtGui.QPen(Qt.black))
+        painter.setBrush(QtGui.QBrush(Qt.red))
 
+        for point in self.snake.snake_array:
+            painter.drawRect(point[0] * SQUARE_SIZE[0],  # Upper left x-coord
+                             point[1] * SQUARE_SIZE[1],  # Upper left y-coord
+                             SQUARE_SIZE[0],            # Width
+                             SQUARE_SIZE[1])            # Height
 
+    def draw_apple(self, painter: QtGui.QPainter) -> None:
+        # Check if there is an apple on the board
+        apple_location = self.snake.apple_location
+        if apple_location:
+            # Init Painter
+            painter.setRenderHint(QtGui.QPainter.HighQualityAntialiasing)
+            painter.setPen(QtGui.QPen(Qt.black))
+            painter.setBrush(QtGui.QBrush(Qt.green))
+
+            painter.drawRect(apple_location[0] * SQUARE_SIZE[0],
+                             apple_location[1] * SQUARE_SIZE[1],
+                             SQUARE_SIZE[0],
+                             SQUARE_SIZE[1])
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)  # Giving it some kind of standard Setup depending on the OS
-    screen = SnakeWidget()
+    screen = SnakeWidget(BOARD_SIZE, SQUARE_SIZE) # Generate new SnakeWidget Object
     screen.show()
     sys.exit(app.exec_()) # Makes sure our window shows up nicely and exits when we want to
